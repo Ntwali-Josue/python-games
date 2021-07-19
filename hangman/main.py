a25704a21e469e1493bbda691eed522211d88992
Ntwali-Josue <kntwalijos@gmail.com>
2021-07-20 01:59:05 +0200

    Create hangman function

--- hangman/main.py
+++ hangman/main.py
@@ -13,5 +13,17 @@ def get_valid_word(words):
 def hangman():
     word = get_valid_word(words)
     words_letters = set(word)
-    alphabet - set(string.ascii_uppercase)
+    alphabet = set(string.ascii_uppercase)
     used_letters = set()
+
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in words_letters:
+            words_letters.remove(user_letter)
+    
+    elif user_letter in used_letters:
+        print('You have already used that character. Please try again.')
+    
+    else:
+        print('Invalid character. Please try again.')
