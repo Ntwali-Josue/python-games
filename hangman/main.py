395d97c77b3dc11c72053f55122f133fa02b7c87
Ntwali-Josue <kntwalijos@gmail.com>
2021-07-20 01:47:48 +0200

    Create get valid word function

---
+++ hangman/main.py
@@ -0,0 +1,14 @@
+import random
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word
+
+
+def hangman():
+    word = get_valid_word(words)
+    words_letters = set(word)
+    alphabet - set(string.ascii_uppercase)
